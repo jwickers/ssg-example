dad9c530f2352fa12a53c7bf9b0274c27d3039af
Jeremy Wickersheimer <jwickers@gmail.com>
2020-10-14 16:37:31 +0800

    Init jamstack example

---
+++ src/js/vibe.js
@@ -0,0 +1,12 @@
+/* eslint-env es6 */
+// some phrases
+const vibes = [
+    ' ... and you are awesome!',
+    ' ... have a greate day!',
+    ' ... and you\'ve got this!',
+    ' ... and so is this puppy! (o^o)'
+];
+
+var vibe = vibes[Math.floor(Math.random() * Math.floor(vibes.length))];
+
+document.querySelector(".vibe").append(vibe)
