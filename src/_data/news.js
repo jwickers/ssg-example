e382f64825967cbf61d5088d5e8bc87579b091ec
Jeremy Wickersheimer <jwickers@gmail.com>
2020-10-14 18:38:56 +0800

    sample using API to get data

---
+++ src/_data/news.js
@@ -0,0 +1,11 @@
+/* eslint-env es6 */
+const axios = require("axios");
+
+module.exports = async function() {
+    try {
+        const response = await axios.get('https://newsapi.org/v2/top-headlines?country=us&apiKey=6f0bbe840cc14057a1b282e0e10678bf&category=technology');
+        return response.data;
+    } catch (err) {
+        console.log(err);
+    }
+}
