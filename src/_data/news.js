c15590ca296a617e3e948feef9e931a957271915
Jeremy Wickersheimer <jwickers@gmail.com>
2020-10-14 18:53:40 +0800

    Use dotenv for API key

--- src/_data/news.js
+++ src/_data/news.js
@@ -1,9 +1,14 @@
 /* eslint-env es6 */
 const axios = require("axios");
+const de = require('dotenv').config();
+
+if (de.error) {
+    throw de.error
+}
 
 module.exports = async function() {
     try {
-        const response = await axios.get('https://newsapi.org/v2/top-headlines?country=us&apiKey=6f0bbe840cc14057a1b282e0e10678bf&category=technology&pageSize=10');
+        const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.NEWSAPI_KEY}&category=technology&pageSize=10`);
         return response.data;
     } catch (err) {
         console.log(err);
