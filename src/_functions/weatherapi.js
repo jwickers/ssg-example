6fee70b685091b573ff2621d65da18ba5682105d
Jeremy Wickersheimer <jwickers@gmail.com>
2020-10-15 12:11:23 +0800

    fix typo

--- src/_functions/weatherapi.js
+++ src/_functions/weatherapi.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 exports.handler = async function(event, context, callback) {
-    const (lat, long ) = event.queryStringParameters;
+    const { lat, long } = event.queryStringParameters;
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${process.env.WEATHERAPI_KEY}&units=metric`;
     const response = await axios.get(url);
     callback(null, {
